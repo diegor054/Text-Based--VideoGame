ffc5107c65a466be68ecf7fadc797873257c1bea
diegor054 <thebrickwall054@gmail.com>
2021-05-12 16:55:43 -0700

    Added a bit of implementation to baseCharacter class

--- header/BaseCharacter.hpp
+++ header/BaseCharacter.hpp
@@ -4,7 +4,28 @@
 
 //implement base class
 
+class BaseCharacter{
+private:
+int health;  
+int attackStrength;
+public:
+//should it be virtual so that each character has their unique attack style??
+virtual void attackOpponent(Opponent* opp) = 0;
 
+void printHealth() const {
+   int lines = health / 10;
+   int totalLines = 10;
+   int spacesLeft = totalLines - lines;
+   cout << "----------" << endl;
+   for(int i = 0; i < lines; i++){
+	cout << "|";
+   }
+   for(int i = 0; i < spacesLeft;i++){
+	cout << " ";
+	}
+   cout << "|  " << health << "remaining health" << endl;
+   cout << "----------" << endl;   
+}
 
-
+};
 #endif
