374001f9d48a189e7aa7b52c98e7fe61ff4cac4e
diegor054 <thebrickwall054@gmail.com>
2021-05-15 22:57:57 -0700

    updated BaseCharacter.hpp

--- header/BaseCharacter.hpp
+++ header/BaseCharacter.hpp
@@ -1,12 +1,16 @@
 #ifndef __BASECHARACTER_HPP__
-#define __BASECHARACTER_HPP_
+#define __BASECHARACTER_HPP__
+
+#include "string"
 
 class BaseCharacter{
 private:
+string name;
 int health;  
 int attackStrength;
 public:
 
+virtual void attack(vector<Opponent*> opp) = 0;
 void printHealth() const {
    int lines = health / 10;
    int totalLines = 10;
