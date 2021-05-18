5c32300a815f011c89920bce53fa6eab1273aab3
zdamm001 <zdamm001@ucr.edu>
2021-05-17 22:03:01 -0700

    Fixes Healer

--- header/Healer.hpp
+++ header/Healer.hpp
@@ -1,27 +1,34 @@
 #ifndef __HEALER_HPP__
 #define __HEALER_HPP__
+
 #include "Player.hpp"
 
-//implement attacker  class
+//implement healer class
 
-class Healer: public Player{
-        private:
-        string wandType; //goes wood, steel, gold, diamond. used just for show not any purpose. propotional to playerTypeLevel
-        public:
-        Healer(): health(200),xp(0), weaponType("Wand"), playerTypeLevel(0), wandType("Wood") {}
-        void Upgrade() override{
-        //implement
-        }
-        void setMaxHealth() const override{
+class Healer: public Player {
+ private:
+    string wandType; //goes wood, steel, gold, diamond. used just for show not any purpose. propotional to playerTypeLevel
+ public:
+    Healer() {
         health = 200;
-        }
-	void setHealth(int x){
-	this->health += x;
-	}
-        void attack(vector<Opponent*> opp) override{
+        xp = 0;
+        weaponType = "Wand";
+        playerTypeLevel = 0;
+        wandType = "Wood";
+    }
+    void Upgrade() override {
         //implement
-        }
-    };
+    }
+    void setMaxHealth() override {
+        health = 200;
+    }
+	void setHealth(int x){
+	    this->health += x;
+	}
+    void attack(vector<Opponent*> opp) override {
+        //implement
+    }
+};
                                                              
 #endif
 
