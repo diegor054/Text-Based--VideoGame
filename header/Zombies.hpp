b0ea5be6974e4034520d7a4ac527ecb733ab5015
diegor054 <thebrickwall054@gmail.com>
2021-05-17 19:06:35 -0700

    Added Zombies Attack Messages

--- header/Zombies.hpp
+++ header/Zombies.hpp
@@ -8,10 +8,23 @@ class Zombies: public Opponent{
 	Zombies(): opponentLevel(1), attackStyle("Fist"), health(25), name("Zombies"), attackStrength(5){}  
 	void attack(vector<Opponent*> opp) override { //the idea of making player index 0 is good but wouldnt the vector have to be BaseCharacters since player isnt an 	//opoenent
 	if(this->health > 0){
-	opponent.at(0).setHealth(-5); 	
-	cout << "A zombie just attacked you and did 5 damage!" << endl;
+	opponent.at(0).setHealth(attackStrength * -1); 	
+	damageMessages(attackStrength);
 	}
 	}
+	void damageMessages(int damage){
+        int num = (rand() % 3) + 1;
+        if(num == 1){
+        cout << "Zombie punched you and took off " << damage << " damage!" << endl;
+        }
+        if(num == 2){
+        cout << "ERRRR. Zombie hit you hard! You lost " << damage << " health!" << endl;;
+        }
+        if(num == 3){
+        cout << "Zombie wants some fresh meat. He attacked you for " << damage << " health!" << endl;
+        }
+        }
+
 
 }
 
