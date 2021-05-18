5f6bb3cf7b64510229d9b5dc32c74b09fbc56603
zdamm001 <zdamm001@ucr.edu>
2021-05-17 19:33:44 -0700

    Edit load function and other changes

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@ void save(const string &, vector<string>*, Character*);
 int main() {
     string file = "data/savefile.txt";
     vector<string>* gameInfo = load(file);
-    if (playerinfo.size() == 0) {
+    if (playerinfo == nullptr) {
         string playerName;
         cout << "Welcome, please enter your player's name: " << flush;
         cin >> playerName;
@@ -19,7 +19,7 @@ int main() {
     
     //run program
     PlayerFactory pf;
-    Character* player = pf.getPlayer(playerInfo);
+    Character* player = pf.getPlayer(gameInfo);
 
     save(file, player);
     return 0;
@@ -34,10 +34,11 @@ vector<string>* load(const string &file) {
     if (fin.eof()) {
         return nullptr;
     }
-    string stage, points, playerInfo, temp;
-    fin << temp << stage;
-    fin << temp << points;
-    fin << temp; getline(fin, playerInfo);
+    string temp;
+    vector<string>* gameInfo = new vector<string>(3);
+    fin << temp << gameInfo->at(0); //stage
+    fin << temp << gameInfo->at(1); //points
+    fin << temp; getline(fin, gameinfo->at(2)); //playerInfo
     fin.close();
     return true;
 }
