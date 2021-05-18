755454979ce34e014ffe514074cff9ea7d8d9296
zdamm001 <zdamm001@ucr.edu>
2021-05-17 21:55:32 -0700

    Fixes Opponent and Goblins

--- header/Goblins.hpp
+++ header/Goblins.hpp
@@ -1,32 +1,40 @@
 #ifndef __GOBLINS_HPP__
 #define __GOBLINS_HPP__
-#include <cstdlib>
-class Goblins: public Opponent{
-        private:
 
-        public:
-        Golbins(): opponentLevel(1), attackStyle("Knife"), health(50), name("Goblins"), attackStrength(2){}
-        void attack(vector<Opponent*> opp) override{
-        if(this->health > 0){
-        opponent.at(0).setHealth(attackStrength * -1);
-        damageMessage(attackStrength);
+#include <iostream>
+#include <cstdlib>
+
+#include "Opponent.hpp"
+
+class Goblins: public Opponent {
+ private:
+
+ public:
+    Goblins() {
+		opponentLevel = 1;
+		attackStyle = "Knife";
+		health = 50;
+		name = "Goblins";
+		attackStrength = 2;
+	}
+    void attack(vector<Opponent*> opp) override {
+    	if(this->health > 0){
+        	opp.at(0)->setHealth(attackStrength * -1);
+        	damageMessages(attackStrength);
         }
 	}
-
-	void damageMessages(int damage){
-	int num = (rand() % 3) + 1;
-	if(num == 1){
-	cout << "Goblem knife you and took off " << damage << " damage!" << endl;
+	void damageMessages(int damage) {
+		int num = (rand() % 3) + 1;
+		if(num == 1) {
+			cout << "Goblem knife you and took off " << damage << " damage!" << endl;
+		}
+		else if(num == 2) {
+			cout << "Goblem hit you hard! You lost " << damage << " health!" << endl;; 
+		}
+		else {
+			cout << "Goblem is very mad at you and attacked you for " << damage << " health!" << endl;
+		}
 	}
-	if(num == 2){
-	cout << "Goblem hit you hard! You lost " << damage << " health!" << endl;; 
-	}
-	if(num == 3){
-	cout << "Goblem is very mad at you and attacked you for " << damage << " health!" << endl;
-	}
-	}	
-}
-
+};
 
 #endif
-
--- header/Opponent.hpp
+++ header/Opponent.hpp
@@ -2,13 +2,12 @@
 #define __OPPONENT_HPP__
 #include "BaseCharacter.hpp"
 
-class Opponent: public BaseCharacter(){
-	private:
+class Opponent: public BaseCharacter {
+ protected:
 	int opponentLevel;
 	string attackStyle;
-	public:
-	Opponent(){}
-	
-
+ public:
+	Opponent() { }
+};
 
 #endif
