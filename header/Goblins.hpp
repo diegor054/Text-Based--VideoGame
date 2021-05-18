d582337f8d2fec05645acd848b2acdbd5164a72d
diegor054 <thebrickwall054@gmail.com>
2021-05-17 19:03:12 -0700

    Added Goblin Attack Messages

--- header/Goblins.hpp
+++ header/Goblins.hpp
@@ -1,5 +1,5 @@
 #ifndef __GOBLINS_HPP__
-#define __GOBlINS_HPP__
+#define __GOBLINS_HPP__
 
 class Goblins: public Opponent{
         private:
@@ -8,11 +8,23 @@ class Goblins: public Opponent{
         Golbins(): opponentLevel(1), attackStyle("Knife"), health(50), name("Goblins"), attackStrength(2){}
         void attack(vector<Opponent*> opp) override{
         if(this->health > 0){
-        opponent.at(0).setHealth(-2);
-        cout << "A Golbin just knifed you and did 2 damage!" << endl;
+        opponent.at(0).setHealth(attackStrength * -1);
+        damageMessage(attackStrength);
         }
-        }
+	}
 
+	void damageMessages(int damage){
+	int num = (rand() % 3) + 1;
+	if(num == 1){
+	cout << "Goblem knife you and took off " << damage << " damage!" << endl;
+	}
+	if(num == 2){
+	cout << "Goblem hit you hard! You lost " << damage << " health!" << endl;; 
+	}
+	if(num == 3){
+	cout << "Goblem is very mad at you and attacked you for " << damage << " health!" << endl;
+	}
+	}	
 }
 
 
