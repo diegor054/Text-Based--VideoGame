2ac39c705f9bfd644d33bd891a757ab692b09ac7
zdamm001 <zdamm001@ucr.edu>
2021-05-17 19:14:23 -0700

    Moved code from main to functions

--- src/main.cpp
+++ src/main.cpp
@@ -4,23 +4,43 @@
 
 using namespace std;
 
+bool load(const string &);
+void save(const string &);
+
 int main() {
     string file = "data/savefile.txt"
+    if (!load(file)) {
+        string playerName;
+        cout << "Welcome, please enter your player's name: " << flush;
+        cin >> playerName;
+    }
+    
+    //run program
+    PlayerFactory pf;
+    Character* player = pf.getPlayer(playerInfo);
+
+    save(file);
+    return 0;
+}
+
+bool load(const string &file) {
     ifstream fin(file);
     if (!fin.is_open()) {
         cout << "Error reading from " << file << endl;
         exit(EXIT_FAILURE);
+    }
+    if (fin.eof()) {
+        return false;
     }
     string stage, points, playerInfo, temp;
     fin << temp << stage;
     fin << temp << points;
     fin << temp; getline(fin, playerInfo);
     fin.close();
-    
-    //run program
-    PlayerFactory pf;
-    Character* player = pf.getPlayer(playerInfo);
+    return true;
+}
 
+void save(const string &file) {
     ofstream fout(file);
     if (!fout.is_open()) {
         cout << "Error writing to " << file << endl;
@@ -30,5 +50,5 @@ int main() {
     fout << "Points: " << numPoints << endl;
     fout << "PlayerInfo: " << player.getType() << endl;
     fout.close();
-    return 0;
+    return true;
 }
