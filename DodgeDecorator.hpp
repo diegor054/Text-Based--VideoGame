a13462d9d94be3e2123565de7a5b8145b4f5ccf3
zdamm001 <zdamm001@ucr.edu>
2021-05-24 01:13:24 -0700

    Added Dodge beginning code

--- DodgeDecorator.hpp
+++ DodgeDecorator.hpp
@@ -6,6 +6,17 @@
 class DodgeDecorator : public AbilityDecorator {
  public:
     DodgeDecorator(BaseCharacter* c) : AbilityDecorator(c) { }
+    virtual void attack(vector<BaseCharacter*> charList) {return c->attack(charList)}
+    virtual void defend(int damage) {
+        if (!(rand() % 2)) {
+            if (!(rand() % 2)) {
+                return c->defend(0);
+            }
+            else {
+                return c->defend(damage/2);
+            }
+        }
+    }
 };
 
 #endif
