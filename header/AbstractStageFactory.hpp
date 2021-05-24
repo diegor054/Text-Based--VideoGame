857b9c2918af1dc09215c0536d61200ff0ebaa3e
zdamm001 <zdamm001@ucr.edu>
2021-05-24 02:10:29 -0700

    Added abstract stage factory

---
+++ header/AbstractStageFactory.hpp
@@ -0,0 +1,25 @@
+#ifndef __ABSTRACT_STAGE_FACTORY__
+#define __ABSTRACT_STAGE_FACTORY__
+
+#include <vector>
+
+using namespace std;
+
+#include "BaseCharacter"
+
+class AbstractStageFactory {
+ public:
+    AbstractStageFactory();
+    virtual vector<BaseCharacter*> getStage1() = 0;
+    virtual vector<BaseCharacter*> getStage2() = 0;
+    virtual vector<BaseCharacter*> getStage3() = 0;
+    virtual vector<BaseCharacter*> getStage4() = 0;
+    virtual vector<BaseCharacter*> getStage5() = 0;
+    virtual vector<BaseCharacter*> getStage6() = 0;
+    virtual vector<BaseCharacter*> getStage7() = 0;
+    virtual vector<BaseCharacter*> getStage8() = 0;
+    virtual vector<BaseCharacter*> getStage9() = 0;
+    virtual vector<BaseCharacter*> getStage10() = 0;
+};
+
+#endif
