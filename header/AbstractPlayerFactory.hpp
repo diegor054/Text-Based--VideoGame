aeb2a8cb48b7c1d84cbd39f1bc0d8a0db857db3d
zdamm001 <zdamm001@ucr.edu>
2021-05-23 22:43:50 -0700

    Changed functions and edited attacker

--- header/AbstractPlayerFactory.hpp
+++ header/AbstractPlayerFactory.hpp
@@ -1,4 +1,20 @@
 #ifndef __ABSTRACT_PLAYER_FACTORY__
 #define __ABSTRACT_PLAYER_FACTORY__
 
+#include <string>
+
+using namespace std;
+
+#include "Player.hpp"
+
+class AbstractPlayerFactory {
+ private:
+    string type;
+ public:
+    AbstractPlayerFactory();
+    virtual Player* getDefaultPlayer() = 0;
+    virtual Player* getUpgradedPlayer(const string &, const string &, const string &) = 0;
+    virtual string getPlayerInfo() = 0;
+};
+
 #endif
--- header/AttackerFactory.hpp
+++ header/AttackerFactory.hpp
@@ -1,4 +1,21 @@
 #ifndef __ATTACKER_FACTORY__
 #define __ATTACKER_FACTORY__
 
+#include "Attacker.hpp"
+#include "AbstractPlayerFactory.hpp"
+
+class AttackerFactory : public AbstractPlayerFactory {
+ private:
+    string type = "Attacker";
+ public:
+    AttackerFactory();
+    virtual Player* getDefaultPlayer() const override {return new Attacker();}
+    virtual Player* getUpgradedPlayer(const string &name, const string &xp, const string &abilityData) const override {
+
+    }
+    virtual string getPlayerInfo() const override {
+
+    }
+};
+
 #endif
