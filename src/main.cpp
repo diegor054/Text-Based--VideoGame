e902d84c21244b7de6c406b6ad29985164ec674f
zdamm001 <zdamm001@ucr.edu>
2021-05-24 02:34:51 -0700

    Added beginning of getPlayer and other fixes

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@ vector<string>* load(const string &);
 void save(const string &, vector<string>*);
 void help(const string &);
 vector<string>* start();
+Player* getPlayer(const string &, const string &, int);
 
 int main() {
     //load program
@@ -23,14 +24,8 @@ int main() {
     }
     
     //run program
-    PlayerFactory pf;
-    Player* player;
-    if (gameInfo->at(3) == "Attacker") player = pf.getAttacker(gameInfo->at(4));
-    else if (gameInfo->at(3) == "Healer") player = pf.getHealer(gameInfo->at(4));
-    else {exit(EXIT_FAILURE);}
     int stage = stoi(gameInfo->at(0));
-    int points = stoi(gameInfo->at(1));
-    player->setName(gameInfo->at(2));
+    Player* player = getPlayer(gameInfo->at(2), gameInfo->at(3), stoi(gameInfo->at(1)));
     
     //save program
     gameInfo->at(2) = player.getType();
@@ -48,12 +43,11 @@ vector<string>* load(const string &file) {
         return nullptr;
     }
     string temp;
-    vector<string>* gameInfo = new vector<string>(5);
+    vector<string>* gameInfo = new vector<string>(4);
     fin >> temp >> gameInfo->at(0); //stage
     fin >> temp >> gameInfo->at(1); //points
     fin >> temp >> gameInfo->at(2); //name
     fin >> temp >> gameInfo->at(3); //type
-    fin >> temp; getline(fin, gameInfo->at(4)); //playerInfo
     fin.close();
     return gameInfo;
 }
@@ -68,7 +62,6 @@ void save(const string &file, vector<string>* gameInfo) {
     fout << "Points: " << gameInfo->at(1) << endl;
     fout << "Name: " << gameInfo->at(2) << endl;
     fout << "Type: " << gameInfo->at(3) << endl;
-    fout << "Abilities: " << gameInfo->at(4) << endl;
     fout.close();
     delete gameInfo;
 }
@@ -88,6 +81,7 @@ vector<string>* start() {
     cout << "Welcome, please enter your player's name: " << flush;
     cin >> playerName;
     help(playerName);
+    cout << endl;
     string playerType = "X";
     bool invalidInput = true;
     while (invalidInput) {
@@ -105,6 +99,13 @@ vector<string>* start() {
             invalidInput = true;
         }
     }
-    return new vector<string>{"0", "0", playerName, playerType, ""};
+    return new vector<string>{"0", "0", playerName, playerType};
 }
 
+Player* getPlayer(const string &name, const string &type, int xp) {
+    AbstractPlayerFactory pf;
+    if (gameInfo->at(3) == "Attacker") pf = new AttackerFactory();
+    else if (gameInfo->at(3) == "Healer") pf = new HealerFactory();
+    else if (gameInfo->at(3) == "Ninja") pf = new NinjaFactory();
+    else return nullptr;
+}
