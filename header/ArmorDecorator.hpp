c0ac33a2acc237cb764f556156d34e18193fe0c2
zdamm001 <zdamm001@ucr.edu>
2021-05-25 13:00:57 -0700

    Fixes Armor Decorator

--- header/ArmorDecorator.hpp
+++ header/ArmorDecorator.hpp
@@ -1,12 +1,14 @@
 #ifndef __ARMOR_DECORATOR__
 #define __ARMOR_DECORATOR__
 
+#include "AbilityDecorator.hpp"
+
 class ArmorDecorator : public AbilityDecorator {
  public:
     ArmorDecorator(BaseCharacter* c) : AbilityDecorator(c) { }
-    virtual BaseCharacter* attack(vector<BaseCharacter*> charList) {return c->attack(charList);}
+    virtual BaseCharacter* attack(vector<BaseCharacter*> charList) {return this->character->attack(charList);}
     virtual int defend(vector<BaseCharacter*> charList, int attackerIndex, int damage) {
-        return c->defend(damage / 2);
+        return character->defend(charList, attackerIndex, damage / 2);
     }
 };
 
