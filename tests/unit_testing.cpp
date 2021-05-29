0e034f6b5a5dbcfc4ed5714c080ff027ee023466
diegor054 <thebrickwall054@gmail.com>
2021-05-29 16:48:54 -0700

    added unit test

--- tests/unit_testing.cpp
+++ tests/unit_testing.cpp
@@ -1,1 +1,23 @@
-TEST
+#include "../header/Attacker.hpp"
+#include "../header/Healer.hpp"
+#include "../header/Ninja.hpp"
+#include "../header/AoeDecorator.hpp"
+#include "../header/ArmorDecorator.hpp"
+#include "../header/CriticalDecorator.hpp"
+#include "../header/DodgeDecorator.hpp"
+#include "../header/DrainDecorator.hpp"
+#include "../header/SpikesDecorator.hpp"
+#include "../header/Zombies.hpp"
+#include "../header/Goblins.hpp"
+#include "../header/Fairies.hpp"
+using namespace std;
+
+TEST(AttackerTest, defaultAttacker) {
+    Attacker* temp = new Attacker();
+    EXPECT_EQ(temp->getHealth(), 100);
+    EXPECT_EQ(temp->getXP() , 0);
+    EXPECT_EQ(temp->getAttackStrength(), 10);
+    EXPECT_EQ(temp->Type(), "Attacker");
+    delete temp;
+}
+
