a0e8221099c100d334843eeaaff5a8f3ee0c946a
diegor054 <thebrickwall054@gmail.com>
2021-05-29 17:09:21 -0700

    added unit test

--- tests/unit_testing.cpp
+++ tests/unit_testing.cpp
@@ -33,6 +33,15 @@ TEST(NinjaTest, defaultNinja) {
     EXPECT_EQ(temp->getHealth(), 200);
     EXPECT_EQ(temp->getXP() , 0);
     EXPECT_EQ(temp->getAttackStrength(), 8);
-    EXPECT_EQ(temp->getType(), "Healer");
+    EXPECT_EQ(temp->getType(), "Ninja");
+    delete temp;
+}
+
+TEST(FairyTest, defaultFairy) {
+    Fairies* temp = new Fairies();
+    EXPECT_EQ(temp->getHealth(), 50);
+    EXPECT_EQ(temp->getXP() , 0);
+    EXPECT_EQ(temp->getAttackStrength(), 3);
+    EXPECT_EQ(temp->getType(), "Fairy");
     delete temp;
 }
