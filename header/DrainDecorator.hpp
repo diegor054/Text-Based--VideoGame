9b5f5e582f79cb00988c51d1a1da1550b52b94b6
zdamm001 <zdamm001@ucr.edu>
2021-05-30 16:57:43 -0700

    Fixes drain level function

--- header/DrainDecorator.hpp
+++ header/DrainDecorator.hpp
@@ -9,8 +9,8 @@ class DrainDecorator : public AbilityDecorator {
     BaseCharacter* attack(vector<BaseCharacter*> charList, int attackerIndex) override {
         BaseCharacter* victim = this->character->attack(charList, attackerIndex);
         if (!(rand() % 3)) {
-            this->character->setHealth(character->getHealth() + (victim->getLatestDamage() * (0.181 + 0.0514 * this->getLevel() + 2.59 * pow(10, -3) * pow(this->getLevel(), 2) + 25)) / 100);
-            addAbilityMessage(this->character->getName() + " managed to gain " + to_string((victim->getLatestDamage() * (0.181 + 0.0514 * this->getLevel() + 2.59 * pow(10, -3) * pow(this->getLevel(), 2) + 25)) / 100) + " from " + victim->getName());
+            this->character->setHealth(character->getHealth() + (victim->getLatestDamage() * (0.1492 + sqrt(0.02512 * this->getLevel() + 0.00573168))/0.01256) / 100);
+            addAbilityMessage(this->character->getName() + " managed to gain " + to_string((victim->getLatestDamage() * (0.1492 + sqrt(0.02512 * this->getLevel() + 0.00573168))/0.01256) / 100) + " from " + victim->getName());
             cout << this->currentMessage << endl;
             this->currentMessage = "";
             return victim;
