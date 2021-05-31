65a30e5f2b022807958ef1c5343a18a643eaa61d
zdamm001 <zdamm001@ucr.edu>
2021-05-30 17:17:36 -0700

    Fixes Critical Decorator bug

--- header/CriticalDecorator.hpp
+++ header/CriticalDecorator.hpp
@@ -7,16 +7,16 @@ class CriticalDecorator : public AbilityDecorator {
  public:
     CriticalDecorator(BaseCharacter* c) : AbilityDecorator(c) { }
     BaseCharacter* attack(vector<BaseCharacter*> charList, int attackerIndex) override {
-        int temp = getAttackStrength();
         if ((rand() % 100 + 1) <= ((0.1492 + sqrt(0.02512 * this->getLevel() + 0.00573168))/0.01256)) {
+            int temp = getAttackStrength();
             setAttackStrength(temp * 1.5);
+            BaseCharacter* victim = this->character->attack(charList, attackerIndex);
+            addAbilityMessage(this->character->getName() + " critically hit " + victim->getName() + "!");
+            cout << this->currentMessage << endl;
+            this->currentMessage = "";
+            setAttackStrength(temp);
         }
-        BaseCharacter* victim = this->character->attack(charList, attackerIndex);
-        addAbilityMessage(this->character->getName() + " critically hit " + victim->getName() + "!");
-        cout << this->currentMessage << endl;
-        this->currentMessage = "";
-        setAttackStrength(temp);
-        return victim;
+        return this->character->attack(charList, attackerIndex);
     }
     virtual int defend(vector<BaseCharacter*> charList, int attackerIndex, int damage) {return this->character->defend(charList, attackerIndex, damage);}
 };
