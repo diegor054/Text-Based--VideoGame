38ad6c5f3b6049abd16f4d124c30195221eb78a5
zdamm001 <zdamm001@ucr.edu>
2021-05-30 23:22:52 -0700

    Fixes Left Path

--- header/RightPathFactory.hpp
+++ header/RightPathFactory.hpp
@@ -6,16 +6,16 @@
 class RightPathFactory : public AbstractStageFactory {
  public:
     RightPathFactory(BaseCharacter* user) : AbstractStageFactory(user) { }
-    virtual vector<BaseCharacter*> getStage1() override {return vector<BaseCharacter*>{player, new Goblins(), new Goblins(), new Goblins()};} // see green moving creature to the right which is why this is goblins
-    virtual vector<BaseCharacter*> getStage2() override {return vector<BaseCharacter*>{player, new Goblins(), new Goblins(), new Goblins(), new Goblins(), new Goblins(), new Goblins()};}  //doubled
-    virtual vector<BaseCharacter*> getStage3() override {return vector<BaseCharacter*>{player, new Goblins(), new Goblins(), new Goblins(), new Goblins(), new Goblins()};} //these will be very strong
-    virtual vector<BaseCharacter*> getStage4() override {return vector<BaseCharacter*>{player, new Fairies(), new Fairies(), new Fairies()};} //time to choose again, they chose right since they were seeing sparkling dust 
+    virtual vector<BaseCharacter*> getStage1() override {return vector<BaseCharacter*>{player, new Goblins(), new Goblins(), new Goblins()};}
+    virtual vector<BaseCharacter*> getStage2() override {return vector<BaseCharacter*>{player, new Goblins(), new Goblins(), new Goblins(), new Goblins(), new Goblins(), new Goblins()};}
+    virtual vector<BaseCharacter*> getStage3() override {return vector<BaseCharacter*>{player, new Goblins(), new Goblins(), new Goblins(), new Goblins(), new Goblins()};}
+    virtual vector<BaseCharacter*> getStage4() override {return vector<BaseCharacter*>{player, new Fairies(), new Fairies(), new Fairies()};} 
     virtual vector<BaseCharacter*> getStage5() override {return vector<BaseCharacter*>{player, new Fairies(), new Fairies(), new Fairies(), new Fairies(), new Fairies(), new Fairies(), new Fairies(), new Fairies()};}
-    virtual vector<BaseCharacter*> getStage6() override {return vector<BaseCharacter*>{player, new Fairies(), new Fairies(), new Fairies(), new Fairies(), new Fairies()};}  // these will be very strong
-    virtual vector<BaseCharacter*> getStage7() override {return vector<BaseCharacter*>{player, new Zombies(), new Zombies(), new Zombies(), new Zombies()};} //hear loud growling and thumping so they went right
-    virtual vector<BaseCharacter*> getStage8() override {return vector<BaseCharacter*>{player, new Zombies(), new Zombies(), new Zombies(), new Zombies(), new Zombies(), new Zombies(), new Zombies()};} //increased to 7
-    virtual vector<BaseCharacter*> getStage9() override {return vector<BaseCharacter*>{player, new Zombies(), new Zombies(), new Zombies(), new Zombies()};} //these will be very strong
-    virtual vector<BaseCharacter*> getStage10() override {return vector<BaseCharacter*>{player, new Goblins()};} //final boss, however we make it
+    virtual vector<BaseCharacter*> getStage6() override {return vector<BaseCharacter*>{player, new Fairies(), new Fairies(), new Fairies(), new Fairies(), new Fairies()};}
+    virtual vector<BaseCharacter*> getStage7() override {return vector<BaseCharacter*>{player, new Zombies(), new Zombies(), new Zombies(), new Zombies()};}
+    virtual vector<BaseCharacter*> getStage8() override {return vector<BaseCharacter*>{player, new Zombies(), new Zombies(), new Zombies(), new Zombies(), new Zombies(), new Zombies(), new Zombies()};}
+    virtual vector<BaseCharacter*> getStage9() override {return vector<BaseCharacter*>{player, new Zombies(), new Zombies(), new Zombies(), new Zombies()};}
+    virtual vector<BaseCharacter*> getStage10() override {return vector<BaseCharacter*>{player, new Goblins()};}
 };
 
 #endif
