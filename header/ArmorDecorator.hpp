20f83ab3a5a132c2e56fda0d76bc2f248a644ef1
diegor054 <thebrickwall054@gmail.com>
2021-05-31 16:43:27 -0700

    updated armorDecorator scripts

--- header/ArmorDecorator.hpp
+++ header/ArmorDecorator.hpp
@@ -11,7 +11,15 @@ class ArmorDecorator : public AbilityDecorator {
     virtual int defend(vector<BaseCharacter*> charList, int attackerIndex, int damage) {
         if (rand() % 2) {
             return character->defend(charList, attackerIndex, (damage * (0.0781 + sqrt(0.01036 * this->getLevel() + 0.0007668))/0.00518) / 100);
+	    int rand = (rand() % 3) + 1;
+	    if(rand == 1){
             addAbilityMessage(charList.at(attackerIndex)->getName() + " faced a wall of armor while trying to hurt " + this->character->getName());
+	    }
+            else if(rand == 2){
+            addAbilityMessage(charList.at(attackerIndex)->getName() + "'s damage was reduced while trying to hurt " + this->character->getName());
+            }else{
+            addAbilityMessage(charList.at(attackerIndex)->getName() + " faced difficulty trying to hurt " + this->character->getName());
+            }     
             cout << this->currentMessage << endl;
             this->currentMessage = "";
         }
