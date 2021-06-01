d46a759b4ce06fe97717f294731bab8d8de67e7e
diegor054 <thebrickwall054@gmail.com>
2021-05-31 17:12:19 -0700

    aoe scripts

--- header/AoeDecorator.hpp
+++ header/AoeDecorator.hpp
@@ -12,7 +12,14 @@ class AoeDecorator : public AbilityDecorator {
         for (vector<BaseCharacter*>::iterator it = charList.begin() + 1; it != charList.end(); ++it) {
             if ((rand() % 100 + 1) <= ((0.0236 + sqrt(0.02512 * this->getLevel() + 0.00573168))/0.01256) && (*it) != opp) {
                 (*it)->defend(charList, 0, this->character->getAttackStrength() / 3);
+                int r = (rand() % 3) + 1;
+                if(r == 1){
                 addAbilityMessage(this->character->getName() + " managed to also hit " + (*it)->getName() + " and deal " + to_string((*it)->getLatestDamage()) + " damage.");
+                }else if(r == 2){
+                addAbilityMessage(this->character->getName() + " managed attack " + (*it)->getName() + " and deal " + to_string((*it)->getLatestDamage()) + " damage too."); 
+               }else{
+                addAbilityMessage(this->character->getName() + " also hit " + (*it)->getName() + " and dealt " + to_string((*it)->getLatestDamage()) + " damage.");
+               }
                 cout << this->currentMessage << endl;
                 this->currentMessage = "";
             }
